9d415d1c65462baf2c61b7a6595ff56773b87c85
Luiz Henrique <luizhjramos@outlook.com>
2022-02-23 18:41:24 -0300

    feat: primary and secondary colors in design system

---
+++ src/styles/index.ts
@@ -0,0 +1,14 @@
+import { createStitches, ScaleValue } from "@stitches/react";
+
+const isThemeDark = true;
+
+export const { styled, getCssText, globalCss } = createStitches({
+  theme: {
+    colors: {
+      bgPrimary: "#1f2028",
+      bgSecondary: "#2e3039",
+      textPrimary: "#fff",
+      textSecondary: "#a9adc1",
+    },
+  },
+});
