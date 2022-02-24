c4e04e8b944d45a1fac4333f8157750d671a4357
Luiz Henrique <luizhjramos@outlook.com>
2022-02-23 22:20:49 -0300

    chore: add react-icons

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -1,18 +1,23 @@
 // Packages
 import React from "react";
 
+// Assets
+import { TiLightbulb } from "react-icons/ti";
+
 // Stitches Components Style
-import { Label, Span, Input } from "./styles"
+import { Label, Span, Input } from "./styles";
 
 const Toggle: React.FC = () => {
   return (
-      <>
-        <Label >
-            <Input type="checkbox" />
-            <Span></Span>
-        </Label>
-      </>
-  )
+    <>
+      <Label>
+        <Input type="checkbox" />
+        <Span>
+          <TiLightbulb />
+        </Span>
+      </Label>
+    </>
+  );
 };
 
 export default Toggle;
