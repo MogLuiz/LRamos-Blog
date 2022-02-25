106d6c8f2dd0ac06c3ee5314522081e1847e8354
Luiz Henrique <luizhjramos@outlook.com>
2022-02-25 20:03:35 -0300

    feat: urql config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,19 +4,29 @@ import { Header } from "../components";
 // Types
 import type { AppProps } from "next/app";
 
+// Providers
+import { Provider } from "urql";
+import { client, ssrCache } from "../lib/urql";
+
 // Stitches Global Style
 import { globalStyles } from "../styles/global";
 
 globalStyles();
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  // Reaproveitando cache do server-side pro client-side
+  if (pageProps.urqlState) {
+    ssrCache.restoreData(pageProps.urqlState);
+  }
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
+      <Provider value={client}>
+        <Header />
+        <Component {...pageProps} />
+      </Provider>
     </>
   );
 };
