7aea7ae54d852603d302ba408474b670c0810c5d
Luiz Henrique <luizhjramos@outlook.com>
2022-03-17 20:28:13 -0300

    feat: Dark styled-component theme

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,49 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  grid: {
+    container: "130rem",
+    gutter: "3.2rem",
+  },
+  border: {
+    full: `99999px`,
+    defaultRadius: `20px`,
+  },
+  font: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    light: 300,
+    normal: 400,
+    bold: 600,
+    sizes: {
+      xsmall: "1.2rem",
+      small: "1.4rem",
+      medium: "1.6rem",
+      large: "1.8rem",
+      xlarge: "2.0rem",
+      xxlarge: "2.8rem",
+    },
+  },
+  colors: {
+    bgPrimary: "#1f2028",
+    bgSecondary: "#2e3039",
+    textPrimary: "#fff",
+    textSecondary: "#a9adc1",
+  },
+  space: {
+    "0": `0px`,
+    "1": `1px`,
+    "4": `0.25rem`,
+    "8": `0.5rem`,
+    "12": `0.75rem`,
+    "16": `1rem`,
+    "24": `1.5rem`,
+    "32": `2rem`,
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50,
+  },
+};
