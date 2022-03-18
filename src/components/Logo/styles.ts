c4800b9d4249af7b1d89ba55e10d396868249d38
Luiz Henrique <luizhjramos@outlook.com>
2022-03-17 21:49:29 -0300

    refact: Logo component refactoring to styled components

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,36 +1,39 @@
-import { styled } from "@/styles";
+// Packages
+import styled, { css } from "styled-components";
 
-export const UlContainer = styled("ul", {
-  position: "relative",
+export const UlContainer = styled.ul`
+  ${({ theme }) => css`
+    position: relative;
 
-  display: "flex",
+    display: flex;
 
-  li: {
-    position: "relative",
-    listStyle: "none",
-  },
-  a: {
-    position: "relative",
-    fontSize: "2.5em",
-    fontWeight: "bold",
-    letterSpacing: "2px",
-    textTransform: "uppercase",
-    color: "transparent",
-    "-webkit-text-stroke": "1px rgba(169, 173, 193, 1)",
-  },
-  "a::before": {
-    content: "attr(data-text)",
-    position: "absolute",
-    color: "$textPrimary",
-    width: "0",
-    overflow: "hidden",
-    transition: "1s",
+    li {
+      position: relative;
+      list-style: none;
+    }
+    a {
+      position: relative;
+      font-size: 2.5em;
+      font-weight: bold;
+      letter-spacing: 2px;
+      text-transform: uppercase;
+      color: transparent;
+      -webkit-text-stroke: 1px rgba(169, 173, 193, 1);
+    }
+    a::before {
+      content: attr(data-text);
+      position: absolute;
+      color: $textPrimary;
+      width: 0;
+      overflow: hidden;
+      transition: 1s;
 
-    borderRight: "8px solid $textPrimary",
-  },
+      border-right: 8px solid ${theme.colors.textPrimary};
+    }
 
-  "a:hover::before": {
-    width: "100%",
-    "-webkit-text-stroke": "1px #fff",
-  },
-});
+    a:hover::before {
+      width: 100%;
+      -webkit-text-stroke: 1px #fff;
+    }
+  `}
+`;
