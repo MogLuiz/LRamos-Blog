8ac52105850cc132a1119d1ce9e8af86294f41db
Luiz Henrique <luizhjramos@outlook.com>
2022-03-18 21:33:54 -0300

    feat: apply font in the project

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,14 +2,43 @@
 import { createGlobalStyle, css } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-    ${({ theme }) => css`
-      * {
-        margin: 0;
-        padding: 0;
-        border: none;
-        box-sizing: border-box;
-      }
 
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/poppins-v19-latin-300.woff2') format('woff2'),
+  }
+
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/poppins-v19-latin-regular.woff2') format('woff2'),
+  }
+
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/poppins-v19-latin-600.woff2') format('woff2'),
+  }
+
+  * {
+      margin: 0;
+      padding: 0;
+      border: none;
+      box-sizing: border-box;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+    }
+    ${({ theme }) => css`
       body {
         background: ${theme.colors.bgPrimary};
       }
@@ -29,6 +58,10 @@ const GlobalStyles = createGlobalStyle`
       a {
         text-decoration: none;
         color: ${theme.colors.textSecondary};
+      }
+
+      body {
+        font-family: ${theme.font.family};
       }
     `}
 `;
