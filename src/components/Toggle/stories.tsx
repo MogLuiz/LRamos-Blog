b387e3e8dc94987854b1cde4aa680627dc926968
Luiz Henrique <luizhjramos@outlook.com>
2022-03-30 21:03:27 -0300

    feat: Storybook Toggle component

---
+++ src/components/Toggle/stories.tsx
@@ -0,0 +1,12 @@
+// Packages
+import { Story, Meta } from "@storybook/react/types-6-0"
+
+// Components
+import Toggle from "."
+
+export default {
+  title: "Toggle",
+  component: Toggle
+} as Meta
+
+export const Default: Story = () => <Toggle />
