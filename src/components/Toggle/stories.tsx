7de1932571b16d425d19674e815a71e179c754c8
Luiz Henrique <luizhjramos@outlook.com>
2022-03-30 21:20:15 -0300

    feat: Toggle Storie parameters

--- src/components/Toggle/stories.tsx
+++ src/components/Toggle/stories.tsx
@@ -1,6 +1,6 @@
 // Packages
 import { Story, Meta } from "@storybook/react/types-6-0"
-
+import styled from "styled-components"
 // Components
 import Toggle from "."
 
@@ -9,4 +9,14 @@ export default {
   component: Toggle
 } as Meta
 
-export const Default: Story = () => <Toggle />
+export const Default: Story = () => (
+  <div style={{ display: "flex", justifyContent: "center" }}>
+    <Toggle />
+  </div>
+)
+
+Default.parameters = {
+  backgounds: {
+    default: "#1F2028"
+  }
+}
